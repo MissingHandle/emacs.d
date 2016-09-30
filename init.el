0772a4bde21e00e77f5f1751e0486bf0793169bb
Gabe Saravia <MissingHandle@users.noreply.github.com>
2016-09-30 08:41:57 -0500

    remove package urls
    this is handled by Cask.

--- init.el
+++ init.el
@@ -10,10 +10,6 @@
 ;;(require 'pallet)
 (require 'package)
 
-(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-                         ("marmalade" . "http://marmalade-repo.org/packages/")
-                         ("melpa" . "http://melpa.milkbox.net/packages/")
-                         ("tromey" . "http://tromey.com/elpa/")))
 (package-initialize)                ;; Initialize & Install Package
 
 ;;
